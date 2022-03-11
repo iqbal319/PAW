1d60c0ce0e78ea967a2a82872efca0af45dc1cb9
iqbal319 <iqbalbaihaqi319@gmail.com>
2022-03-12 00:35:17 +0700

    menambahkan file hello.php

---
+++ Tugas2/hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello php</title>
+</head>
+<body>
+    <?php
+    $Nama="Moh. Iqbal Baihaki"; //Nama
+    $NIM="200411100004"; //NIM
+    $Alamat="Jl. Raya Talun No. 130 Sumberrejo Bojonegoro"; //Alamat
+
+    echo"Nama :$Nama <br>";
+    echo"NIM : $NIM <br>";
+    echo"Alamat : $Alamat <br>";
+    ?>
+</body>
+</html>
