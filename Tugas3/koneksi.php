8deacd45cf7b9ca35444ff966865f73459380dc1
iqbal319 <iqbalbaihaqi319@gmail.com>
2022-04-02 17:45:21 +0700

    file bootsrap

--- Tugas3/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//Koneksi Database
-	$server = "localhost";
-	$user = "root";
-	$pass = "iqbal140103";
-	$database = "iqbal";
-
-	$koneksi = mysqli_connect($server, $user, $pass, $database)or die(mysqli_error($koneksi));
-?>
