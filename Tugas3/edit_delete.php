9af3cdb0ca5317ecb8c5a77d8d949f4f713659e7
iqbal319 <iqbalbaihaqi319@gmail.com>
2022-04-02 21:39:00 +0700

    menambahkan file edit_delete.php

---
+++ Tugas3/edit_delete.php
@@ -0,0 +1,38 @@
+<?php
+
+	include = "koneksi.php";
+
+		//Pengujian jika tombol Edit / Hapus di klik
+	if(isset($_GET['hal']))
+	{
+		//Pengujian jika edit Data
+		if($_GET['hal'] == "edit")
+		{
+			//Tampilkan Data yang akan diedit
+			$tampil = mysqli_query($koneksi, "SELECT * FROM tb_004 WHERE id_mhs = '$_GET[id]' ");
+			$data = mysqli_fetch_array($tampil);
+			if($data)
+			{
+				//Jika data ditemukan, maka data ditampung ke dalam variabel
+				$vnim = $data['nim'];
+				$vnama = $data['nama'];
+				$valamat = $data['alamat'];
+				$vprodi = $data['prodi'];
+			}
+		}
+		else if ($_GET['hal'] == "hapus")
+		{
+			//Persiapan hapus data
+			$hapus = mysqli_query($koneksi, "DELETE FROM tb_004 WHERE id_mhs = '$_GET[id]' ");
+			if($hapus){
+				echo "<script>
+						alert('Hapus Data Suksess!!');
+						document.location='index.php';
+				     </script>";
+			}
+		}
+	}
+
+
+
+?>
