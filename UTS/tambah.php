4122c816aa7838037185353f0d8bbcb729f62614
iqbal319 <iqbalbaihaqi319@gmail.com>
2022-04-18 07:10:51 +0700

    file tambah.php

---
+++ UTS/tambah.php
@@ -0,0 +1,67 @@
+<?php include('config.php'); ?>
+
+		<center><font size="6">Tambah Data</font></center>
+		<hr>
+		<?php
+		if(isset($_POST['submit'])){
+			$id				= $_POST['id'];
+			$nama			= $_POST['nama'];
+			$ukuran			= $_POST['ukuran'];
+			$harga			= $_POST['harga'];
+			$deskripsi		= $_POST['deskripsi'];
+
+			$cek = mysqli_query($koneksi, "SELECT * FROM produk WHERE id='$id'") or die(mysqli_error($koneksi));
+
+			if(mysqli_num_rows($cek) == 0){
+				$sql = mysqli_query($koneksi, "INSERT INTO produk(id, nama, ukuran, harga, deskripsi) VALUES('$id', '$nama', '$ukuran', '$harga', 
+					'$deskripsi')") or die(mysqli_error($koneksi));
+
+				if($sql){
+					echo '<script>alert("Berhasil menambahkan data."); document.location="index.php?page=tampil_prdk";</script>';
+				}else{
+					echo '<div class="alert alert-warning">Gagal melakukan proses tambah data.</div>';
+				}
+			}else{
+				echo '<div class="alert alert-warning">Gagal, ID sudah terdaftar.</div>';
+			}
+		}
+		?>
+
+		<form action="index.php?page=tambah_prdk" method="post">
+			<div class="item form-group">
+				<label class="col-form-label col-md-3 col-sm-3 label-align">ID</label>
+				<div class="col-md-6 col-sm-6 ">
+					<input type="text" name="id" class="form-control" size="4" required>
+				</div>
+			</div>
+			<div class="item form-group">
+				<label class="col-form-label col-md-3 col-sm-3 label-align">Nama Produk</label>
+				<div class="col-md-6 col-sm-6">
+					<input type="text" name="nama" class="form-control" required>
+				</div>
+			</div>
+			<div class="item form-group">
+				<label class="col-form-label col-md-3 col-sm-3 label-align">Ukuran</label>
+				<div class="col-md-6 col-sm-6">
+					<input type="text" name="ukuran" class="form-control" required>
+				</div>
+			</div>
+			<div class="item form-group">
+				<label class="col-form-label col-md-3 col-sm-3 label-align">Harga</label>
+				<div class="col-md-6 col-sm-6">
+					<input type="text" name="harga" class="form-control" required>
+				</div>
+			</div>
+			<div class="item form-group">
+				<label class="col-form-label col-md-3 col-sm-3 label-align">Deskripsi</label>
+				<div class="col-md-6 col-sm-6">
+					<input type="text" name="deskripsi" class="form-control" required>
+				</div>
+			</div>
+			
+			<div class="item form-group">
+				<div  class="col-md-6 col-sm-6 offset-md-3">
+					<input type="submit" name="submit" class="btn btn-primary" value="Simpan">
+			</div>
+		</form>
+	</div>
