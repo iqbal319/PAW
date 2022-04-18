4e3118a0d559ed8526632bf812e387e10dc588bb
iqbal319 <iqbalbaihaqi319@gmail.com>
2022-04-18 07:07:38 +0700

    file home

---
+++ UTS/home.php
@@ -0,0 +1,4 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/toko.png" width="200px height="200px" /> <br>
+<font Size="6" face="Helvetica">Welcome to Administrator</font> <br>
+</center>
